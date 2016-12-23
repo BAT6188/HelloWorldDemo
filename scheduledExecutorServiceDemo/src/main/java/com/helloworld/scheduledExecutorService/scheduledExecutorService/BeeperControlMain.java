c38837530e6ee62ad4fb9224bc8237032ddd167c
zane29 <zhouhaiming0828@gmail.com>
2016-12-23 15:55:48 +0800

    scheduledExecutorServiceDemo

---
+++ scheduledExecutorServiceDemo/src/main/java/com/helloworld/scheduledExecutorService/scheduledExecutorService/BeeperControlMain.java
@@ -0,0 +1,44 @@
+package com.helloworld.scheduledExecutorService.scheduledExecutorService;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 周海明
+ * Date: 2016/12/14
+ * Time: 15:19
+ */
+
+/**
+ * 计划定时任务
+ *JDKDemo
+ * */
+public class BeeperControlMain {
+    private final ScheduledExecutorService scheduler =
+            Executors.newScheduledThreadPool(1);
+
+    public static void main(String[] args) {
+        BeeperControlMain beeperControl = new BeeperControlMain();
+        beeperControl.beepForAnHour();
+    }
+
+    public void beepForAnHour() {
+        final Runnable beeper = new Runnable() {
+            public void run() {
+                System.out.println("我是小蜜蜂..beep");
+            }
+        };
+        final ScheduledFuture<?> beeperHandle =
+                scheduler.scheduleAtFixedRate(beeper, 10, 10, SECONDS);
+        scheduler.schedule(new Runnable() {
+            public void run() {
+                beeperHandle.cancel(true);
+            }
+        }, 60 * 60, SECONDS);
+    }
+
+}
